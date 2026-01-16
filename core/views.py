ee1e3e8dbd091ad4d4b58a69a7e9162015765b31
aawamry <a.awamry@live.com>
2026-01-16 14:53:47 +0200

    Extending DataSetView Class to Perform Create

--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+import pandas as pd
 from .models import Dataset, Variable
 from .serializers import DatasetSerializer, VariableSerializer
 
@@ -7,6 +8,18 @@ from .serializers import DatasetSerializer, VariableSerializer
 class DatasetViewSet(viewsets.ModelViewSet):
     queryset = Dataset.objects.all()
     serializer_class = DatasetSerializer
+
+    def perform_create(self, serializer):
+        dataset=serializer.save(uploaded_by=self.request.user)
+        #Parse CSV
+        df = pd.read_csv(dataset.file.path)
+        for col in df.columns:
+            Variable.objects.create(
+                dataset=dataset,
+                name=col,
+                type=str(df[col].dtype),
+            )
+
 
 class VariableViewSet(viewsets.ModelViewSet):
     queryset = Variable.objects.all()
